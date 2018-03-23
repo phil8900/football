c0daf4d3f04b4a683ecc3a4f36ca5787643be0d8
Philip <hankep@cip.ifi.lmu.de>
2018-03-23 15:35:43 +0100

    Further ranking logic added

--- public_html/js/ranking.js
+++ public_html/js/ranking.js
@@ -1,14 +1,52 @@
 var database = firebase.database();
 
-getRanking('rankings/users/');
-getRanking('rankings/teams/');
+var userRanking;
+var teamRanking;
+var ownteam = 0;
+var uid;
+var userRef = firebase.database().ref('rankings/users/').orderByChild('points');
+var teamRef = firebase.database().ref('rankings/teams/').orderByChild('points');
 
-function getRanking(childstring){
+firebase.auth().onAuthStateChanged(function(user) {
+	if (user) {
+		uid = user.uid;
+	} else {
+		uid = 'AwTsR03Y7LRpsRiz5RaCojUwhqy2';
+	}
+});
+
+userRef.on('value', function(snapshot) {
 	var array = new Array();
-	var teams = database.ref().child(childstring).orderByChild('points').once('value').then(function(snapshot) {
-		snapshot.forEach(function(child) {
-		array.push(child.val());
-		});
+	snapshot.forEach(function(child) {
+		if(child.val().uid == uid){
+			ownteam = child.val().team;
+		}
+		if(child.val().team == ownteam){
+			array.push(child.val());
+		}
 	});
-	return array;
+
+	array.sort(function(a, b){return b.points-a.points});
+	userRanking = array;
+	setTeamRanking(userRanking);
+});
+
+teamRef.on('value', function(snapshot) {
+	var array = new Array();
+	snapshot.forEach(function(child) {
+			array.push(child.val());
+	});
+	array.sort(function(a, b){return b.points-a.points});
+	teamRanking = array;
+});
+
+function setTeamRanking(userRanking){
+	var ranking = 0;
+	userRanking.forEach(function(entry) {
+    ranking += entry.points;
+});
+	firebase.database().ref('rankings/teams/' + ownteam).set({
+    points: ranking,
+    teamid:ownteam
+  });
 }
