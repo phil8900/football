c19a5185ce354d436ee8a5543c60e9a77bdb6675
Philip Hanke <hankep@cip.ifi.lmu.de>
2018-04-11 17:16:07 +0200

    Code indention

--- public_html/js/terms.js
+++ public_html/js/terms.js
@@ -1,40 +1,37 @@
 redirect();
 
 function redirect(){
-		firebase.auth().onAuthStateChanged(function(user) {
-			var userRef = firebase.database().ref('rankings/users/' + user.uid + '/terms');
-				userRef.once('value').then(function(snapshot) {
-				if(snapshot.val()['terms'] == 'true'){
-					window.location = "match.php";
-				}
-				else{
-					console.log('here');
-					var e = document.createElement('div');
-					var acceptbutton = document.createElement('button');
-            		var acceptbuttontext = document.createTextNode("Accept");
-            		acceptbutton.appendChild(acceptbuttontext);
+	firebase.auth().onAuthStateChanged(function(user) {
+		var userRef = firebase.database().ref('rankings/users/' + user.uid + '/terms');
+		userRef.once('value').then(function(snapshot) {
+			if(snapshot.val()['terms'] == 'true'){
+				window.location = "match.php";
+			}
+			else{
+				var e = document.createElement('div');
+				var acceptbutton = document.createElement('button');
+            	var acceptbuttontext = document.createTextNode("Accept");
+            	acceptbutton.appendChild(acceptbuttontext);
 
-            		acceptbutton.addEventListener("click", function(){
-            		firebase.database().ref('rankings/users/' + user.uid + '/terms').set({
-            			terms: 'true'
-            		});
-          			window.location = "match.php";
-      });
+           		acceptbutton.addEventListener("click", function(){
+           		firebase.database().ref('rankings/users/' + user.uid + '/terms').set({
+           			terms: 'true'
+           		});
+           		window.location = "match.php";
+           	});
 
-					var declinebutton = document.createElement('button');
-            		var declinebuttontext = document.createTextNode("Decline");
-            		declinebutton.appendChild(declinebuttontext);
+           		var declinebutton = document.createElement('button');
+           		var declinebuttontext = document.createTextNode("Decline");
+           		declinebutton.appendChild(declinebuttontext);
 
-            		declinebutton.addEventListener("click", function(){
-            			alert('You need to accept the T&Cs');
-            		});
+           		declinebutton.addEventListener("click", function(){
+           			alert('You need to accept the T&Cs');
+           		});
 
-            		e.appendChild(acceptbutton);
-            		e.appendChild(declinebutton);
-            		document.getElementById('buttons').appendChild(e);
-				}
+            	e.appendChild(acceptbutton);
+            	e.appendChild(declinebutton);
+            	document.getElementById('buttons').appendChild(e);
+            }
+        });
 	});
-
-});
-	
 }
