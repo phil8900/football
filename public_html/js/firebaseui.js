71765b6a08d3ba7b8b64690c296a435abc4ee215
Philip Hanke <hankep@cip.ifi.lmu.de>
2018-04-11 17:12:45 +0200

    Terms and conditions

--- public_html/js/firebaseui.js
+++ public_html/js/firebaseui.js
@@ -24,7 +24,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: '<url-to-redirect-to-on-success>',
+  signInSuccessUrl: 'terms.php',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -33,7 +33,7 @@ var uiConfig = {
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
   ],
   // Terms of service url.
-  tosUrl: '<your-tos-url>'
+  tosUrl: 'terms.php'
 };
 
 // The start method will wait until the DOM is loaded.
---
+++ public_html/terms.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<?php include 'header.php';?>
+<script src="js/terms.js"></script>
+</head>
+
+<body>
+<!-- The surrounding HTML is left untouched by FirebaseUI.
+     Your app may use that space for branding, controls and other customizations.-->
+<h1>AU Football</h1>
+<h2>Terms and conditions</h2>
+<p>Read carefully, because Philip wants all your data.</p>
+<div id="buttons"></div>
+</body>
+</html>
