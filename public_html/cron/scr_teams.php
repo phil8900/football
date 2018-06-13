0bfed08ed809da77808c9185f7fcd431ff6b6085
Philip Hanke <hankep@cip.ifi.lmu.de>
2018-06-13 16:20:47 +0200

    Fixed competition url

--- public_html/cron/scr_teams.php
+++ public_html/cron/scr_teams.php
@@ -3,7 +3,7 @@ require 'scraper.php';
 
 echo 'Started teamscraper';
 
-$competition_url = 'https://www.transfermarkt.co.uk/premier-league/startseite/pokalwettbewerb/WM18';
+$competition_url = 'https://www.transfermarkt.co.uk/weltmeisterschaft-2018/teilnehmer/pokalwettbewerb/WM18/saison_id/2017';
 scrapeTeams(getTeamURLsForCompetition($competition_url));
 
 ?>
